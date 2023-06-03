fca457c6cf8e886eacfde45da4bd2c35830e104d
jguerreiro <joel.borghi@gmail.com>
2023-06-03 14:33:47 -0300

    Changing meta tags

--- components/meta.js
+++ components/meta.js
@@ -34,9 +34,10 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`The Learned Feline | A Wise Cat's Blog`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta property="og:title" content={`The Learned Feline Blog`} />
     </Head>
   )
 }
