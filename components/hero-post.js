cb9f7a66733395cb258d2e420f5b7455c2fa0eec
jguerreiro <joel.borghi@gmail.com>
2023-06-03 13:56:02 -0300

    Personalizing the template

--- components/hero-post.js
+++ components/hero-post.js
@@ -13,10 +13,10 @@ export default function HeroPost({
 }) {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
+      <div className="mb-8 md:mb-16 p-4">
         <CoverImage title={title} slug={slug} url={coverImage.url} />
       </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28 p-4">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
             <Link href={`/posts/${slug}`} className="hover:underline">
